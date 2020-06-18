c706b1aaea11d06b814db762efb120cfae244839
Beroni <beronimagalhaes@gmail.com>
2020-06-18 07:52:37 -0300

    Add global Style

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,19 +1,25 @@
-import Document, { DocumentContext } from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+import Document, { DocumentContext } from "next/document";
+import { ServerStyleSheet } from "styled-components";
+import GlobalStyle from "../src/styles/global";
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
           enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        })
+            sheet.collectStyles(
+              <>
+                <GlobalStyle />
+                <App {...props}></App>
+              </>
+            ),
+        });
 
-      const initialProps = await Document.getInitialProps(ctx)
+      const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
         styles: (
@@ -22,9 +28,9 @@ export default class MyDocument extends Document {
             {sheet.getStyleElement()}
           </>
         ),
-      }
+      };
     } finally {
-      sheet.seal()
+      sheet.seal();
     }
   }
 }
